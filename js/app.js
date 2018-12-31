82e92d2ae16e3fc751632dcb077dcb6480f1c069
yukiIchika <yuki.midting.ichika@gmail.com>
2018-12-31 23:09:35 +0900

    correction

--- js/app.js
+++ js/app.js
@@ -14,9 +14,9 @@ const wh = window.innerHeight
 const ww = window.parent.screen.width
 const hh = document.getElementById("canvas-container").clientHeight
 const ttest = `${hh} ${wh} ${(hh - wh) / 2}`
-alert("test")
+alert("test2")
 //alert(ttest)
-//document.getElementById("canvas-container").style.bottom = `${(hh - wh) / 2}px`
+document.getElementById("canvas-container").style.bottom = `-${(hh - wh) / 2}px`
 
 // Worldの生成
 const createEngine = parentNode => {
