994779e333bec71e0caae2927d6ccfc0f1f26413
yukiIchika <yuki.midting.ichika@gmail.com>
2018-12-31 22:47:04 +0900

    correction

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ const alphaToWhite = data8U => {
 
 const wh = window.innerHeight
 const ww = window.parent.screen.width
-const hh = document.getElementById("canvas-container").outerHeight()
+const hh = document.getElementById("canvas-container").clientHeight
 const ttest = `${hh} ${wh} ${(hh - wh) / 2}`
 alert(ttest)
 //document.getElementById("canvas-container").style.top = `${(window.parent.screen.height - wh) / 2}px`
